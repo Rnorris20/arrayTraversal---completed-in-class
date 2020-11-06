b23d6fde2c4ac4b71bcfdc4fe70855f324585283
RNorris6 <ryan.norris@patelhs.org>
2020-11-06 00:10:55 +0000

    Fixed errors between the ending brackets and the semi colon after print statements.

--- Main.java
+++ Main.java
@@ -2,17 +2,14 @@ class Main {
   public static void main(String[] args) {
     String[] candy = {"candy corn", "dots", "twix", "skittles", "starbursts", "hershey's", "jolly ranchers"};
 
-  for(int i = 0; i < 7; i++);
+  for(int i = 0; i < 7; i++){
     System.out.println("The candy is " + candy[i]);
   }
   
   candy[6] = "kit kat";
 
-  for(int i = 0; i < 7; i++);
+  for(int i = 0; i < 7; i++){
     System.out.println("The update " + candy[i]);
-  }
-
-  //create a loop that loops backwards through the candy array.
 
     }
   }
